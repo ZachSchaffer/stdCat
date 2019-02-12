4ee5737d0e3c590336a1f91c5bd5edb1fa4d817d
Zach Schaffer <cursedhydra@gmail.com>
2019-02-11 19:14:06 -0500

    update cat

--- stdcat.c
+++ stdcat.c
@@ -2,20 +2,20 @@
 
 int main(int argc, char *argv[]) {
 
-    if (argc == 2) {
-        FILE *in;
-        char *fileName = argv[1];
-        if ((in = fopen(fileName, "r")) == NULL) {
-            printf("Cannot read or open %s.\n", fileName);
-            return 1;
-        }
-        char c;
-        while ((c = (char) getc(in)) != EOF) {
-            printf("%c", c);
-        }
+  FILE *in;
+  char *fileName = argv[1];
 
-        fclose(in);
-    }
+  if ((in = fopen(fileName, "r")) == NULL) {
+    printf("Cannot read or open %s.\n", fileName);
+    return 1;
+  }
+  char c;
 
-    return 0;
+  while ((c = (char)getc(in)) != EOF) {
+    printf("%c", c);
+  }
+
+  fclose(in);
+
+  return 0;
 }
